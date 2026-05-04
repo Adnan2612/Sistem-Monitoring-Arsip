ac04f551b56c081db82e0e5739b6b42475f37048
Adnan2612 <h1051221013@student.untan.ac.id>
2026-05-04 09:44:27 +0700

    Update .gitignore, package.json, vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";   // ← Tailwind v4 plugin
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/Sistem-Monitoring-Arsip/", 
   plugins: [
     react(),
     tailwindcss(),
